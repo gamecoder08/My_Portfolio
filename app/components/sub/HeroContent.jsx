131f2e932d58296f3a1a0a359e2091c54e416bca
gamecoder08 <gamecoder0811@gmail.com>
2025-11-12 21:06:39 +0530

    Update: added about me

--- app/components/sub/HeroContent.jsx
+++ app/components/sub/HeroContent.jsx
@@ -1,67 +1,76 @@
 "use client";
-import React from 'react';
-import {motion} from 'framer-motion';
-import { slideInFromLeft, slideInFromRight, slideInFromTop } from 'public/utils/motion';
-import { SparklesIcon } from '@heroicons/react/24/solid';
-import Image from 'next/image';
+import React from "react";
+import { motion } from "framer-motion";
+import {
+    slideInFromLeft,
+    slideInFromRight,
+    slideInFromTop,
+} from "public/utils/motion";
+import { SparklesIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
 
 const HeroContent = () => {
     return (
         <motion.div
-        initial='hidden'
-        animate='visible'
-        className='flex flex-row items-center justify-center px-20 mt-40 w-full z-20'
+            initial="hidden"
+            animate="visible"
+            className="flex flex-row items-center justify-center px-20 mt-40 w-full z-20"
         >
-            <div className='h-full w-full flex flex-col gap-5 justify-center m-auto text-start'>
+            <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
                 <motion.div
-                variants={slideInFromTop}
-                className='Welcome-box py-[15px] px-[7px] border border-[#7042f88b] opacity-[0.9]'
+                    variants={slideInFromTop}
+                    className="Welcome-box py-[15px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
                 >
-
-                
-                <SparklesIcon className='text-[#b49bff] mr-2.5 h-5 w-5 '/>
-                <h1 className='Welcome-text text-[13px]'>Software Developer Engineer Portfolio</h1>
-
+                    <SparklesIcon className="text-[#b49bff] mr-2.5 h-5 w-5 " />
+                    <h1 className="Welcome-text text-[13px]">
+                        Software Developer Engineer Portfolio
+                    </h1>
                 </motion.div>
 
                 <motion.div
-                variants={slideInFromLeft(0.5)}
-                className='flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto'
+                    variants={slideInFromLeft(0.5)}
+                    className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
                 >
                     <span>
-                    Providing
-                    <span className='text-transparent bg-clip-text bg-linear-to-r from-purple-500 to-cyan-500'> the best </span>
-                    project experience
+                        Providing
+                        <span className="text-transparent bg-clip-text bg-linear-to-r from-purple-500 to-cyan-500">
+                            {" "}
+                            the best{" "}
+                        </span>
+                        project experience
                     </span>
                 </motion.div>
 
                 <motion.p
-                variants={slideInFromLeft(0.7)}
-                className='text-lg text-gray-400 my-5 max-w-[600px]'
+                    variants={slideInFromLeft(0.7)}
+                    className="text-lg text-gray-400 text-justify my-5 max-w-[600px]"
                 >
-                    I am a passionate adaptive software developer dedicated to crafting innovative and efficient solutions. Explore my portfolio to see how I can bring value to your projects.
+                    I am a passionate software developer lost in the world of coding,
+                    constantly seeking to create innovative solutions and bring ideas to
+                    life through technology. Explore my portfolio to see how I
+                    can bring value to your projects.
                 </motion.p>
                 <motion.a
-                variants={slideInFromLeft(1)}
-                className='py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]'
+                    variants={slideInFromLeft(1)}
+                    className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
                 >
                     Learn More!
                 </motion.a>
             </div>
 
             <motion.div
-            variants={slideInFromRight(0.7)}
-            className='w-full h-full flex justify-center items-center'
+                variants={slideInFromRight(0.7)}
+                className="w-full h-full flex justify-center items-center"
             >
                 <Image
-                src="/images/mainIconsdark.svg"
-                alt="Work Icons"
-                height={650}
-                width={650}
+                    src="/images/mainIconsdark.svg"
+                    alt="Work Icons"
+                    height={650}
+                    width={650}
                 />
             </motion.div>
         </motion.div>
-    )
-}
+    );
+};
 
-export default HeroContent
+export default HeroContent;
--- app/page.jsx
+++ app/page.jsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import Hero from "./components/landing/Hero";
 import Skills from "./components/landing/Skills";
 import Projects from "./components/landing/Projects";
+import AboutMe from "./components/landing/AboutMe";
 
 export default function Home() {
   return (
     <main className="h-full w-full">
       <div className="flex flex-col gap-20">
         <Hero />
+        <AboutMe />
         <Skills />
         <Projects />
       </div>
